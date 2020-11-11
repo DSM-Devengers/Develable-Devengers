f0a3fce7f5c3b2ad696867972a83bb3343027403
Seogunhee <seogunhee4@gmail.com>
2020-11-11 09:39:15 +0900

    [ADD]기본서버 구조

---
+++ src/main/java/devengers/develable/develable_server/DevelableServerApplication.java
@@ -0,0 +1,13 @@
+package devengers.develable.develable_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelableServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevelableServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/devengers/develable/develable_server/DevelableServerApplicationTests.java
@@ -0,0 +1,13 @@
+package devengers.develable.develable_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelableServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
